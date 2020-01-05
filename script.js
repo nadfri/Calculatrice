657977bf8660a0cec8bb7f4e5cb51bb0d07caac9
Nader Frigui <nadfri@gmail.com>
2020-01-06 00:45:07 +0100

    Style CSS Bureau OK

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 window.onload = function() {
 
 //--------Déclaration varaibles
-    const idAdd = document.getElementById("addition");
-    const idSous = document.getElementById("soustraction");
-    const idMulti = document.getElementById("multiplication");
-    const idDivis = document.getElementById("division");
-    const idResultat = document.getElementById("resultat");
-    const idSpan1 = document.getElementById("span1");
-    const idSpan2 = document.getElementById("span2");
+    const idAdd         = document.getElementById("addition");
+    const idSous        = document.getElementById("soustraction");
+    const idMulti       = document.getElementById("multiplication");
+    const idDivis       = document.getElementById("division");
+    const idClear       = document.getElementById("clear");
+    const idResultat    = document.getElementById("resultat");
+    const idSpan1       = document.getElementById("span1");
+    const idSpan2       = document.getElementById("span2");
+    const idDivInvalide = document.getElementById("invalide");
+
 
     let idNombre1 = document.getElementById("nombre1");
     let idNombre2 = document.getElementById("nombre2");
 
 //--Verification input
     let verif = (a, b) => {
-        if (a == "" || b == "") return false;
+        if (a == "" || b == "") 
+        {
+            idDivInvalide.style.visibility = "visible";
+            return false;
+        }
         else 
+        {   
+            idDivInvalide.style.visibility = "hidden";
         	return true;
+        }
     };
 
 //----Operateurs------------------
@@ -24,8 +34,8 @@ window.onload = function() {
 
         if ( verif(a, b) ) 
         {
-            let resultat = parseInt(a) + parseInt(b);
-            idResultat.innerHTML += `<p>${a} + ${b} = ${resultat} <p>`;
+            let resultat = parseFloat(a) + parseFloat(b);
+            idResultat.innerHTML += `<p>${a} <span>➕</span> ${b} = ${resultat} <p>`;
         }
     };
 
@@ -33,8 +43,8 @@ window.onload = function() {
 
         if (verif(a, b))
         {
-            let resultat = parseInt(a) - parseInt(b);
-            idResultat.innerHTML += `<p>${a} - ${b} = ${resultat} <p>`;
+            let resultat = parseFloat(a) - parseFloat(b);
+            idResultat.innerHTML += `<p>${a} <span>➖</span> ${b} = ${resultat} <p>`;
     	}
     };
 
@@ -42,8 +52,8 @@ window.onload = function() {
 
         if (verif(a, b))
         {
-            let resultat = parseInt(a) * parseInt(b);
-            idResultat.innerHTML += `<p>${a} x ${b} = ${resultat} <p>`;
+            let resultat = parseFloat(a) * parseFloat(b);
+            idResultat.innerHTML += `<p>${a} <span>✖️</span> ${b} = ${resultat} <p>`;
         }
     };
 
@@ -52,8 +62,10 @@ window.onload = function() {
 
         if (verif(a, b))
         {
-        	let resultat = parseInt(a) / parseInt(b);
-        	idResultat.innerHTML += `<p>${a} / ${b} = ${resultat} <p>`; 
+        	let resultat = parseFloat(a) / parseFloat(b);
+            if(b=="0") idResultat.innerHTML +=`<p>${a} / ${b} = <i>division par 0 impossible</i> <p>`;
+
+        	else idResultat.innerHTML += `<p>${a} <span>➗</span> ${b} = ${resultat} <p>`; 
         }
     };
     
@@ -74,32 +86,36 @@ window.onload = function() {
         division(idNombre1.value, idNombre2.value);
     };
 
+    idClear.onclick = () => {idResultat.innerHTML="";};
+
+
+
+
 
 //----Verifcation en direct de la saisie Nombre
 	const verifEnLive = (nombre,span) =>
 		{
-			let regex = /\d/;
+			let regex = /^\d*.?\d+$/;
 	        if (!regex.test(nombre.value))
 	        {
-	        	span.style.display = "inline";
-	        	span.style.color = "red";
-	    		span.innerHTML   = "❌ <i>Entrez un nombre valide</i>";
+	    		span.innerHTML   = "❌";
+                idDivInvalide.style.visibility = "visible";
+
 	    	}
 	        else
 	        {    
-	    		span.style.display = "inline";
-	    		span.style.color = "green";
 	    		span.innerHTML   = " ✔️";
+                idDivInvalide.style.visibility = "hidden";
 	    	}
 		};
 
 	idNombre1.onkeyup = () => {verifEnLive(idNombre1, idSpan1); };
     idNombre2.onkeyup = () => {verifEnLive(idNombre2, idSpan2); };
 
-//-----Focus sur l'element survolé
+//-----Selection de la valeur sur l'element survolé
 
-	idNombre1.onmouseover = () => {idNombre1.focus();};
-	idNombre2.onmouseover = () => {idNombre2.focus();};
+	idNombre1.onmouseover = () => {idNombre1.select(); };
+	idNombre2.onmouseover = () => {idNombre2.select(); };
 
 
 	
